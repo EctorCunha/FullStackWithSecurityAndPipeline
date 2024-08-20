453ec3f0a515dafb927d61eb849999cd7db43f3c
Ector Cunha <ectorvccunha@outlook.com>
2024-08-20 20:26:50 -0300

    corrigindo a entrada de diretorio pela pipeline2

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,8 +2,10 @@ import { app } from './app'
 import mongoose from 'mongoose';
 
 
+const secret = process.env.SECRET_DB
+
 mongoose.set("strictQuery", false);
-mongoose.connect(`mongodb+srv://ectorcunha:ectorcunha@fullstack.zkdai.mongodb.net/?retryWrites=true&w=majority&appName=FullStack`)
+mongoose.connect(`mongodb+srv://ectorcunha:${secret}@fullstack.zkdai.mongodb.net/?retryWrites=true&w=majority&appName=FullStack`)
 .then(() => {
   console.log('MongoDB Conectado')
   app.listen(5000);
